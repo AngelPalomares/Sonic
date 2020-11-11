db767298695e9e721490c8b529fdb269dc665dfe
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2020-11-11 13:47:16 -0600

    Main

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+    public float JumpForce;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+
+    private bool canDoubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, .2f, whatIsGround);
+
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, JumpForce);
+            }
+            else
+            {
+                if(canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, JumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
+    }
+}

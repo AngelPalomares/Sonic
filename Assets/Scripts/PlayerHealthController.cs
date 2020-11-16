f29352f202323d8d8edb391f031a8fc45d00e458
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2020-11-16 11:33:24 -0600

    Update

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+    public int MaxHealth;
+    public int CurrentHealth;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Deals Damage to Player
+    public void DealDamage()
+    {
+        CurrentHealth--;
+
+        if(CurrentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
